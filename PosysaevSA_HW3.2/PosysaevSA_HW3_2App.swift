e328fcfb4e8f6ce3224cd716aad4e51e69449703
SergeiPosysaev <zilogfps@yandex.ru>
2020-12-13 22:47:27 +0300

    Initial Commit

---
+++ PosysaevSA_HW3.2/PosysaevSA_HW3_2App.swift
@@ -0,0 +1,17 @@
+//
+//  PosysaevSA_HW3_2App.swift
+//  PosysaevSA_HW3.2
+//
+//  Created by Sergei Posysaev on 13.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct PosysaevSA_HW3_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
